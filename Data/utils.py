290c299de44b8511ee4600f79e975e1376ab567a
akatoch1 <akshatkatoch@yahoo.com>
2023-11-04 17:01:54 -0400

    updated utils

--- Data/utils.py
+++ Data/utils.py
@@ -1,37 +1,16 @@
 import re
 import os
-import torch
-import clip
 
-directory = 'INSERT DIRECTORY PATH'
-classes = []
+
 #get all classes
-for filename in os.listdir(directory):
-    if filename.endswith(".jpg"):  
-      delimiters = '[.-]'
-      result = re.split(delimiters, filename)
-      result = [item for item in result if item]
-      classes.append(result[2])
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model, preprocess = clip.load('ViT-B/32', device) #load clip model
+def get_all_classes(directory):
+    for filename in os.listdir(directory):
+        if filename.endswith(".jpg"):  
+            delimiters = '[.-]'
+        result = re.split(delimiters, filename)
+        result = [item for item in result if item]
+        classes.append(result[2])
+path = 'INSERT DIRECTORY PATH'
+#classes = get_all_classes(path)
 
 
-for i in range(len(classes)):
-  classes[i] = clip.tokenize(classes[i]) #tokenize all classes
-text_inputs = torch.cat(classes).to(device)
-
-def image_CLIP(image, text, m):
-  
-  with torch.no_grad():
-    image_features = m.encode_image(image)
-    text_features = m.encode_text(text)
-  image_features /= image_features.norm(dim=-1, keepdim=True)
-  text_features /= text_features.norm(dim=-1, keepdim=True)
-  similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
-  values, indices = similarity[0].topk(1)
-  return classes[indices[0]] # return predicted class
-'''USE FUNCTION ON EACH IMAGE TO GET PREDICTED IMAGE
-Like this:  
-for each image:
-  predicted_class = image_clip(image, text_inputs, model)
-'''
--- model/layer/tokenization.py
+++ model/layer/tokenization.py
@@ -5,23 +5,15 @@ import sys
 sys.path.append('../clip')
 from model import *
 class Tokenize:
-    def __init__(self, path):
-        self.directory = path
-        self.classes = []
-    #get all classes
-    def get_class_tokens(self):
-        clip = 
-        for filename in os.listdir(self.directory):
-            if filename.endswith(".jpg"):  
-                delimiters = '[.-]'
-            result = re.split(delimiters, filename)
-            result = [item for item in result if item]
-            self.classes.append(result[2])
-        device = "cuda" if torch.cuda.is_available() else "cpu"
-        for i in range(len(self.classes)):
-            self.classes[i] = clip.tokenize(self.classes[i]) #tokenize all classes
-        text_inputs = torch.cat(classes).to(device)
 
-        return self.classes
+    def image_CLIP(image, text, m, classes):
+        with torch.no_grad():
+            image_features = m.encode_image(image)
+            text_features = m.encode_text(text)
+        image_features /= image_features.norm(dim=-1, keepdim=True)
+        text_features /= text_features.norm(dim=-1, keepdim=True)
+        similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+        values, indices = similarity[0].topk(1)
+        return classes[indices[0]] # return predicted class
 
 
